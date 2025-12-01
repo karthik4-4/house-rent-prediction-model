cd5853a8e5edd22eaad0907027949f18f6157de2
karthik <thalakantikarthikkumarreddy@gmail.com>
2025-12-01 22:39:48 +0530

    installed req...

---
+++ README.md
@@ -0,0 +1,33 @@
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```bash
+https://github.com/karthik4-4/house-rent-prediction-model
+```
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -n myenv python=3.9 -y
+```
+
+```bash
+conda activate myenv
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up you local host and port
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_desc = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = 'house-rent-prediction-model'
+AUTHOR_USER_NAME = 'karthik4-4'
+SRC_REPO = 'House-Rent-Prediction-model'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    description="python package for project",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
